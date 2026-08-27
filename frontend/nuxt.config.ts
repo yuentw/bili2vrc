4cc3e3097b110f4d872ac4b16dba97a0933b38ba
Mio Nekoya <admin@mio9.sh>
2026-08-28 02:53:50 +0900

    disable nuxt telemetry

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   ssr: false,
   devtools: { enabled: true },
+  telemetry: false,
   nitro: {
     devProxy: {
       '/api': { target: 'http://127.0.0.1:5000', changeOrigin: true },
